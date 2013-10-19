2e7ea62c8ab18d69e9b9ae0262e3cb9d77329f78
jhlive <jhlive300590@gmail.com>
2013-10-18 18:34:01 -0600

    firs commit rails rumble

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrwoudlySound::Application.config.secret_key_base = '2261a71b05167a993245ce3b8b145f76cab153fef15c2f08642267165f2dd00daa79b2350fd08c55d1f86da717c26f69d51543e6bfdf1cd47d30501bc8e63131'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CrwoudlySound::Application.config.session_store :cookie_store, key: '_crwoudly_sound_session'
