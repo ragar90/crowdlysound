1117a007c34075f04745799ddfb491d73d9943bd
Pablo Marti <pablo@greenmediaagency.com>
2013-10-18 23:50:46 -0600

    Main controller for feed and landing page with Guest

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,16 @@
+class MainController < ApplicationController
+
+  before_filter :check_musician, except: [:landing_page, :login_as_guest]
+  
+  def landing_page
+  end
+
+  def login_as_guest
+  	session[:musician_id] = 0
+  	redirect_to root_path
+  end
+
+  def index
+  end
+
+end
