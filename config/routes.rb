f4290b6c8e4e83c9ad823fbddd1dec4006db30bc
Pablo Marti <pablo@greenmediaagency.com>
2013-10-20 17:08:32 -0600

    Rollback

--- config/routes.rb
+++ config/routes.rb
@@ -33,8 +33,12 @@ CrwoudlySound::Application.routes.draw do
   get 'follow_band' => 'bands#follow_band'
 
   resources :songs do
-    member do
+    collection do
       get "castings" => "songs#castings", as: :castings
+      #get "avaliable_for_castings" => "songs#avaliable_for_castings", as: :castings_avalibles
+    end
+
+    member do
       put "casting/:casting_id/approve/for/:instrument_id" => "songs#change_casting_status", as: :aprove_casting, defaults: { status: 1 }
       put "casting/:casting_id/reject/for/:instrument_id" => "songs#change_casting_status", as: :reject_castin, defaults: { status: 2 }    
       post "cover" => "songs#cover", as: :cover
