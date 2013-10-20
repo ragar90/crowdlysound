8f29ce4fe20fdeb33b56a0a44f4ff7f66579c1b9
Pablo Marti <pablo@greenmediaagency.com>
2013-10-20 11:17:37 -0600

    Rocks for songs and music sheets

--- app/controllers/rocks_controller.rb
+++ app/controllers/rocks_controller.rb
@@ -3,7 +3,7 @@ class RocksController < ApplicationController
 	def update
 		content_id = params[:content_id]
 		content_type = params[:content_type]
-		action = params[:action].to_i
+		action = params[:action_value].to_i
 
 		if params[:content_type] == "Song"
 			@item = Song.find(params[:content_id])
@@ -11,7 +11,8 @@ class RocksController < ApplicationController
 			@item = MusicSheet.find(params[:content_id])
 		end
 
-		@item.rocks_by(@current_musician, action)
+		logger.debug "CM: #{@current_musician.inspect}"
+		@item.rocks_by(@current_musician.id, action)
 	end
 
 end
